12d930460f2651af9c9f5f9c71313380242e43b8
GeertKock <g.kock1@student.avans.nl>
2021-12-14 12:29:35 +0100

    [Add] basic for recyclerview

--- TDMD/app/src/main/java/com/example/tdmd/MainActivity.kt
+++ TDMD/app/src/main/java/com/example/tdmd/MainActivity.kt
@@ -4,14 +4,19 @@ import android.app.Fragment
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    var recyclerView: RecyclerView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val firstFragment = FirstFragment();
         val secondFragment = SecondFragment();
+
+        //val recyclerAdapter = RecyclerAdapter(this, "Pikachu", "Electric type pokemon", 11, "HOI")
 
         supportFragmentManager.beginTransaction().apply {
             replace(R.id.flFragment, firstFragment)
--- TDMD/app/src/main/java/com/example/tdmd/SecondFragment.java
+++ TDMD/app/src/main/java/com/example/tdmd/SecondFragment.java
@@ -3,6 +3,7 @@ package com.example.tdmd;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,9 +12,11 @@ import android.view.ViewGroup;
 import com.example.tdmd.R;
 
 public class SecondFragment extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_second, container, false);
     }
